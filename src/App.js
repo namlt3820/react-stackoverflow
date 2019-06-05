3b64e1fa49183aa02a26b23a9d0f7136eaeb4882
thanhthinh.mai <thinh.mai@vmodev.com>
2019-06-05 10:08:36 +0700

    delete folders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from "react";
 import User from "./components/User";
 import Home from "./components/Home/Home";
-import Login from "./components/Auth/Login";
-import SignUp from "./components/Auth/SignUp";
-import ForgotPassWord from "./components/Auth/ForgotPassWord";
-import ActiveCode from "./components/Auth/ActiveCode";
-import ChangPassWord from "./components/Auth/ChangPassWord";
-import QuestionList from './components/Questions/QuestionList'
-import TopicList from './components/Topics/TopicList'
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import ForgotPassWord from "./pages/Auth/ForgotPassWord";
+import ActiveCode from "./pages/Auth/ActiveCode";
+import ChangPassWord from "./pages/Auth/ChangPassWord";
+// import QuestionList from './components/Questions/QuestionList'
+// import TopicList from './components/Topics/TopicList'
 import MyTopic from "./components/MyTopic/MyTopic";
 import MyQuestion from "./components/MyQuestion/MyQuestion";
-import Main from "./components/Layout/Main";
-import TagList from './components/Tags'
+import Main from "./layout/Main";
+import TagList from "./components/Tags";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./general.css";
 import "./App.css";
 import AskQuestion from "./components/AskQuestion/AskQuestion";
-
-
-
-
 
 function App() {
     return (
@@ -34,9 +30,9 @@ function App() {
                         <Route path="/active" component={ActiveCode} />
                         <Route path="/change-password" component={ChangPassWord} />
                         <Route path="/my-profile" component={User} />
-                        <Route path="/ask-question" component={AskQuestion}/>
-                        <Route path="/my-questions" component={MyQuestion}/>
-                        <Route path="/my-topics" component={MyTopic}/>
+                        <Route path="/ask-question" component={AskQuestion} />
+                        <Route path="/my-questions" component={MyQuestion} />
+                        <Route path="/my-topics" component={MyTopic} />
                         <Route path="/tags" component={TagList} />
                     </Switch>
                 </Main>
--- src/components/Tags/Tag.js
+++ src/components/Tags/Tag.js
@@ -6,32 +6,20 @@ import { Col, Card } from "antd";
 
 const ResponsiveEllipsis = responsiveHOC()(LinesEllipsis);
 const Tag = ({ tag: { name, description, questions, followers } }) => (
-  <Col
-    md={{ span: 6 }}
-    xs={{ span: 24 }}
-    sm={{ span: 12 }}
-    style={{ marginTop: "1rem" }}
-  >
-    <Card
-      title={name}
-      extra={
-        <Tooltip title={`${questions} Questions, ${followers} Followers`}>
-          <a href="#" style={{ fontSize: "0.9rem" }}>
-            {questions} <i className="fa fa-question" />, {followers}{" "}
-            <i className="fa fa-user" />
-          </a>
-        </Tooltip>
-      }
-    >
-      <ResponsiveEllipsis
-        text={description}
-        maxLine="3"
-        ellipsis="..."
-        trimRight
-        basedOn="letters"
-      />
-    </Card>
-  </Col>
+    <Col md={{ span: 6 }} xs={{ span: 24 }} sm={{ span: 12 }} style={{ marginTop: "1rem" }}>
+        <Card
+            title={name}
+            extra={
+                <Tooltip title={`${questions} Questions, ${followers} Followers`}>
+                    <a href="#" style={{ fontSize: "0.9rem" }}>
+                        {questions} <i className="fa fa-question" />, {followers} <i className="fa fa-user" />
+                    </a>
+                </Tooltip>
+            }
+        >
+            <ResponsiveEllipsis text={description} maxLine="3" ellipsis="..." trimRight basedOn="letters" />
+        </Card>
+    </Col>
 );
 
 export default Tag;

590a747c9942959bfb4237eb2e7623438f85034c
thanhthinh.mai <thinh.mai@vmodev.com>
2019-06-07 18:09:43 +0700

    fix import file

--- src/components/QuestionDetails/QuestionInPageDetail.js
+++ src/components/QuestionDetails/QuestionInPageDetail.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
-import TitleQuestion from './TitleQuestion';
-import QuestionContent from '../MyQuestion/QuestionContent';
-import LikeReplay from './LikeReplay';
+import React, { Component } from "react";
+import TitleQuestion from "./TitleQuestion";
+import QuestionContent from "../MyQuestion/MyQuestion";
+import LikeReplay from "./LikeReplay";
 
 class QuestionInPageDetail extends Component {
     render() {
         return (
             <div>
                 <div className="container-fluid">
-                <div className="row">
-                <div className="col">
-                <div className="card mt-2 mb-2">
-                <TitleQuestion/>
-                <QuestionContent/>
-                <hr className="non-margin"/>
-                <LikeReplay/>
+                    <div className="row">
+                        <div className="col">
+                            <div className="card mt-2 mb-2">
+                                <TitleQuestion />
+                                <QuestionContent />
+                                <hr className="non-margin" />
+                                <LikeReplay />
+                            </div>
+                        </div>
+                    </div>
                 </div>
-               
-            </div>
-                </div>
-            </div>
-                
             </div>
         );
     }

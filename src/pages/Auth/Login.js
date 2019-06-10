53e5d5389ace9e021c4acdde483d873b688a0eb7
thanhthinh.mai <thinh.mai@vmodev.com>
2019-06-10 16:08:59 +0700

    add function for input and button

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
-import CustomInputAuth from "../../components/Cores/input/CustomInputAuth";
 import CustomButton from "../../components/Cores/button/CustomButton";
 import Facebook from "../../components/Social/FaceBook";
 import GoogleLogin from "../../components/Social/Google";
+import Lable from "../../components/Cores/lable/Lable";
+import Input from "../../components/Cores/input/Input";
 import "./style.css";
 
 class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            loading: false
+            loading: false,
+            email: "",
+            password: "",
+            errors: {}
         };
-        this.handleLogin = this.handleLogin.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
-    handleLogin() {
+    handleClick() {
         this.setState({
             loading: true
         });
@@ -21,9 +26,18 @@ class Login extends Component {
             this.setState({ loading: false });
         }, 3000);
     }
+    handleChange(e) {
+        e.preventDefault();
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
     render() {
-        let { loading } = this.state;
+        let { loading, email, password } = this.state;
         const { match } = this.props;
+        const formLogin = [
+            { lable: "Email", type: "email", name: "email", value: email },
+            { lable: "Password", type: "password", name: "password", value: password }
+        ];
         return (
             <div className="auth-page">
                 <div className="social">
@@ -31,9 +45,18 @@ class Login extends Component {
                     <Facebook />
                 </div>
                 <div className="login">
-                    <CustomInputAuth lable="Email" type="email" />
-                    <CustomInputAuth lable="Password" type="password" match={match} />
-                    <CustomButton content="Login" handleClick={this.handleLogin} loading={loading} />
+                    {formLogin.map((item, index) => (
+                        <div className="mb-3" key={index}>
+                            <Lable lable={item.lable} match={match} />
+                            <Input
+                                type={item.type}
+                                name={item.name}
+                                value={item.value}
+                                handleChange={this.handleChange}
+                            />
+                        </div>
+                    ))}
+                    <CustomButton content="Login" handleClick={this.handleClick} loading={loading} />
                 </div>
             </div>
         );
